2cbb2515c8862792bc6f1d4212c72f15f25a2aff
drashti-1310 <drashtikahodariya64036@gmail.com>
2022-01-05 21:02:42 +0530

    delete and update

--- lib/Controller/todo_controller.dart
+++ lib/Controller/todo_controller.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-import 'package:sqflite/sqflite.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqlite_todo/Helpers/constant.dart';
 import 'package:sqlite_todo/Model/todoModel.dart';
 
@@ -12,8 +11,8 @@ class TodoController extends GetxController {
     String path = join(await getDatabasesPath(), databaseName);
     await openDatabase(path, version: version,
         onCreate: (Database db, int version) async {
-          printInfo(info: "db:"+ db.toString());
-          await db.execute("CREATE TABLE $tblName ("
+      printInfo(info: "db:" + db.toString());
+      await db.execute("CREATE TABLE $tblName ("
           "$columnId TEXT PRIMARY KEY,"
           "$columnTitle TEXT NOT NULL,"
           "$columnDescription TEXT NOT NULL,"
@@ -22,34 +21,55 @@ class TodoController extends GetxController {
     });
     getTodo();
   }
-  void addData({required String description,required String date,required String title})
-  {
-    var todo=TodoModel(
-      id:(todoList.length+ 1).toString(),
-      title: title,
-      description: description,
-      date: date
-    );
+
+  void addData(
+      {required String description,
+      required String date,
+      required String title}) {
+    var todo = TodoModel(
+        id: (todoList.length + 1).toString(),
+        title: title,
+        description: description,
+        date: date);
     insertData(todo);
   }
-  Future<void> insertData(TodoModel todo)
-  async {
+
+  Future<void> insertData(TodoModel todo) async {
     String path = join(await getDatabasesPath(), databaseName);
-    final db=await openDatabase(path);
+    final db = await openDatabase(path);
 
     await db.insert(tblName, todo.toJson());
     print(todo.toJson());
     getTodo();
-
   }
-  Future<void> getTodo()
-  async {
+
+  Future<void> getTodo() async {
     todoList.clear();
     String path = join(await getDatabasesPath(), databaseName);
-  final db=await openDatabase(path);
+    final db = await openDatabase(path, version: version);
 
-  final List<Map<String,dynamic>> maps= await db.query(tblName);
-  todoList.addAll(maps.map((e) => TodoModel.fromJson(e)).toList());
+    final List<Map<String, dynamic>> maps = await db.query(tblName);
+    todoList.addAll(maps.map((e) => TodoModel.fromJson(e)).toList());
+  }
 
+  delete({String? id}) async {
+    String path = join(await getDatabasesPath(), databaseName);
+    final db = await openDatabase(path, version: version);
+    getTodo();
+    return await db.delete(tblName, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  updateTodo(TodoModel todo) async {
+    print(todo.id);
+    String path = join(await getDatabasesPath(), databaseName);
+    final db = await openDatabase(path);
+    await db.update(
+      tblName,
+      todo.toJson(),
+      where: 'id = ?',
+      whereArgs: [todo.id],
+    );
+    getTodo();
+    return todo;
   }
 }
--- lib/Screens/dashboard.dart
+++ lib/Screens/dashboard.dart
@@ -5,6 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:sizer/sizer.dart';
 import 'package:sqlite_todo/Controller/todo_controller.dart';
 import 'package:sqlite_todo/Helpers/constant.dart';
+import 'package:sqlite_todo/Model/todoModel.dart';
 
 final todoController = Get.put(TodoController());
 
@@ -46,60 +47,91 @@ class _DashboardState extends State<Dashboard> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView.separated(
-          itemCount: todoController.todoList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 12.h,
-              color: Colors.brown,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Text(
-                      "${todoController.todoList[index].id}",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      width: 10.w,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "${todoController.todoList[index].title}",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400),
+        child: Obx(
+          () => todoController.todoList.length == 0
+              ? CircularProgressIndicator()
+              : ListView.separated(
+                  itemCount: todoController.todoList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Dismissible(
+                      background: Container(
+                        color: Colors.green,
+                      ),
+                      key: ValueKey(index),
+                      onDismissed: (DismissDirection direction) {
+                        todoController.delete(
+                            id: todoController.todoList[index].id);
+                      },
+                      child: GestureDetector(
+                        onTap: () {
+                          showModalBottomSheet<void>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return ModelBottomSheet(
+                                todoDetail: todoController.todoList[index],
+                              );
+                            },
+                          );
+                        },
+                        child: Container(
+                          height: 12.h,
+                          color: Colors.brown,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  "${todoController.todoList[index].id}",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  width: 10.w,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "${todoController.todoList[index].title}",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                    Text(
+                                      "${todoController.todoList[index].description}",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                    Text(
+                                      "${todoController.todoList[index].date}",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w400),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 40.w,
+                                ),
+                                Icon(Icons.auto_delete,
+                                    size: 30, color: Colors.redAccent),
+                              ],
+                            ),
+                          ),
                         ),
-                        Text(
-                          "${todoController.todoList[index].description}",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400),
-                        ),
-                        Text(
-                          "${todoController.todoList[index].date}",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400),
-                        )
-                      ],
-                    )
-                  ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Container(height: 2.h);
+                  },
                 ),
-              ),
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return Container(height: 2.h);
-          },
         ),
       ),
     );
@@ -107,8 +139,8 @@ class _DashboardState extends State<Dashboard> {
 }
 
 class ModelBottomSheet extends StatefulWidget {
-  const ModelBottomSheet({Key? key}) : super(key: key);
-
+  ModelBottomSheet({Key? key, this.todoDetail}) : super(key: key);
+  final TodoModel? todoDetail;
   @override
   _ModelBottomSheetState createState() => _ModelBottomSheetState();
 }
@@ -118,96 +150,121 @@ class _ModelBottomSheetState extends State<ModelBottomSheet> {
   final _descriptionController = TextEditingController();
   final _dateController = TextEditingController();
   GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  @override
+  void initState() {
+    super.initState();
+    if (widget.todoDetail != null) {
+      _titleController.text = widget.todoDetail!.title!;
+      _descriptionController.text = widget.todoDetail!.description!;
+      _dateController.text = widget.todoDetail!.date!;
+    }
+  }
+
+  void dispose() {
+    super.dispose();
+    _titleController.clear();
+    _descriptionController.clear();
+    _dateController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50.h,
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Form(
-              key: formkey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextFormField(
-                    controller: _titleController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter tittle";
-                      }
-                    },
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        labelText: "Tittle"),
-                  ),
-                  SizedBox(
-                    height: 2.h,
-                  ),
-                  TextFormField(
-                    controller: _descriptionController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter description";
-                      }
-                    },
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        labelText: "Description"),
-                  ),
-                  SizedBox(
-                    height: 2.h,
-                  ),
-                  TextFormField(
-                    readOnly: true,
-                    controller: _dateController,
-                    onTap: () => _selectDate(),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Select Date";
-                      }
-                    },
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        labelText: "Date"),
-                  ),
-                  SizedBox(
-                    height: 2.h,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      if (formkey.currentState!.validate()) {
-                        Get.back();
-                        todoController.addData(
-                            title: _titleController.text,
-                            description: _descriptionController.text,
-                            date: _dateController.text);
-                      }
-                    },
-                    child: Container(
-                      height: 6.h,
-                      width: 15.h,
-                      decoration: BoxDecoration(
-                        color: teal,
-                      ),
-                      child: Center(
-                          child: Text(
-                        "ADD",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      )),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: 50.h,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: formkey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _titleController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter tittle";
+                        }
+                      },
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          labelText: "Tittle"),
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    TextFormField(
+                      controller: _descriptionController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter description";
+                        }
+                      },
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          labelText: "Description"),
+                    ),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    TextFormField(
+                      readOnly: true,
+                      controller: _dateController,
+                      onTap: () => _selectDate(),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Select Date";
+                        }
+                      },
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          labelText: "Date"),
+                    ),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        if (formkey.currentState!.validate()) {
+                          Get.back();
+                          if (widget.todoDetail != null) {
+                            todoController.updateTodo(TodoModel(
+                                id: widget.todoDetail!.id,
+                                title: _titleController.text,
+                                description: _descriptionController.text,
+                                date: _dateController.text));
+                          } else {
+                            todoController.addData(
+                                title: _titleController.text,
+                                description: _descriptionController.text,
+                                date: _dateController.text);
+                          }
+                        }
+                      },
+                      child: Container(
+                        height: 6.h,
+                        width: 15.h,
+                        decoration: BoxDecoration(
+                          color: teal,
+                        ),
+                        child: Center(
+                            child: Text(
+                          widget.todoDetail == null ? "ADD" : "UPDATE",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
